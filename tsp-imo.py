b610a796ee6708e9cc44eff9d1338e4cee34c8e0
unknown <jjkasprzak@protonmail.com>
2022-03-20 21:12:46 +0100

    show best solution

--- tsp-imo.py
+++ tsp-imo.py
@@ -13,8 +13,17 @@ if __name__ == '__main__':
         for algorithm in ['nn', 'gc', '2r', '3r']:
             print(filename + ' ' + algorithm)
             scores=[]
+            bestSolution=None
+            bestScore=None
             for i in range(1000):
                 solver.solve(instance, algorithm, visualize=False)
                 scores.append(instance.score())
+                if bestScore == None or scores[-1] < bestScore:
+                    bestScore=scores[-1]
+                    bestSolution=instance.solution
+            
             print(str(sum(scores)//len(scores)) + ' (' + str(min(scores)) + ' - ' + str(max(scores)) + ')')
-            #instance.show()
+            if bestScore:
+                print(bestScore)
+                instance.solution=bestSolution
+                instance.show()
